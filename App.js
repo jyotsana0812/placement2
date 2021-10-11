6ef822fe8897c42e3c1f6a5892854cc5c8f011a6
Your Name <[ashutosh.singh3889@gmail.com]>
2021-10-11 23:28:54 +0530

    my second commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1>Learn React with Redux</h1>  
+        <h2>COUNTER:0</h2>
+        <button>+</button>
+        <button>-</button>
+        
+      
+    </div>
+  );
+}
+
+export default App;
